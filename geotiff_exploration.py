463578716140149c76bf6df8d5751ed00ab6ec85
allison-forsyth <aforsyt4@gmu.edu>
2025-02-17 20:33:21 -0500

    got pre and post flood images and converted them into a large pandas dataframe. exploring what to do next with vv and vh values from the geotiff

--- geotiff_exploration.py
+++ geotiff_exploration.py
@@ -1,158 +1,107 @@
-
+""""
+Import all necessary packages
+"""
 import rasterio
 import rasterio.plot
 import matplotlib
 import rioxarray as rxr 
 
-file = 'test_tiff.tiff'
-tiff = rasterio.open(file)
+""""
+read pre and post flood images
+"""
+preflood_VV = 'flood_images_10m_resolution/preflood_VV_decibel_gamma0_2017-09-09.tiff'
+preflood_VH = 'flood_images_10m_resolution/preflood_VH_decibel_gamma0_2017-09-09.tiff'
+postflood_VV= 'flood_images_10m_resolution/postflood_VV_decibel_gamma0_2017-09-22.tiff'
+postflood_VH = 'flood_images_10m_resolution/postflood_VH_decibel_gamma0_2017-09-22.tiff'
 
 
+preflood_VVtiff = rasterio.open(preflood_VV)
+preflood_VHtiff = rasterio.open(preflood_VH)
+postflood_VVtiff = rasterio.open(postflood_VV)
+postflood_VHtiff = rasterio.open(postflood_VH)
 
-rasterio.plot.show(tiff)
+""""
+code to explore plotting and inspection of tiff files if necessary
+"""
 
-print(tiff.shape)
-#band 1 = red
-#band 2= green
-#band 3 = blue
-num_bands = tiff.count
-print(f"Number of bands: {num_bands}")
+#rasterio.plot.show(tiff)
 
-band_indexes = tiff.indexes
-print(f"Band indexes: {band_indexes}")
+# print(tiff.shape)
 
-band_dtypes = tiff.dtypes
-print(f"Band data types: {band_dtypes}")
+# num_bands = tiff.count
+# print(f"Number of bands: {num_bands}")
 
+# band_indexes = tiff.indexes
+# print(f"Band indexes: {band_indexes}")
 
-# Open the GeoTIFF file
-with rasterio.open(file) as src:
-    # Read a specific band (e.g., band 1)
-    band_number = 1
-    band_data = src.read(band_number)
+# band_dtypes = tiff.dtypes
+# print(f"Band data types: {band_dtypes}")
 
-band_data
+""""
+read data from a band example
+"""
 
-with rasterio.open(file) as src:
-    # Read a specific band (e.g., band 1)
-    band_number = 2
-    band_data2 = src.read(band_number)
+# # Open the GeoTIFF file
+# with rasterio.open(file) as src:
+#     # Read a specific band (e.g., band 1)
+#     band_number = 1
+#     band_data = src.read(band_number)
 
-band_data2
-
-with rasterio.open(file) as src:
-    # Read a specific band (e.g., band 1)
-    band_number = 3
-    band_data3 = src.read(band_number)
-
-band_data3
-
-tiff
-
-for i in range(1, num_bands + 1):
-            band_metadata = tiff.tags(i)
-            print(f"Band {i} metadata: {band_metadata}")
-
-file2 = 'dorado_09222017/2017-09-22-00:00_2017-09-22-23:59_Sentinel-1_IW_VV+VH_VH_-_decibel_gamma0.tiff'
-tiff2 = rasterio.open(file2)
-
-rasterio.plot.show(tiff2)
-
-print(tiff2.shape)
-
-num_bands = tiff2.count
-print(f"Number of bands: {num_bands}")
-
-band_indexes = tiff2.indexes
-print(f"Band indexes: {band_indexes}")
-
-band_dtypes = tiff2.dtypes
-print(f"Band data types: {band_dtypes}")
-
-with rasterio.open(file2) as src:
-    # Read a specific band (e.g., band 1)
-    band_number = 1
-    band_data = src.read(band_number)
-
-band_data
-
-
-# file2='dorado_09222017/2017-09-22-00:00_2017-09-22-23:59_Sentinel-1_IW_VV+VH_VH_-_decibel_gamma0.tiff'
-# df2 = rxr.open_rasterio(file2,
-#                                  masked=True)
-# df2.rio.bounds()
-
-# #3 bands in decible gamma
-# print("Number of bands", df2.rio.count)
+# band_data
 
 #different bands to pandas
 import pandas as pd
-da = rxr.open_rasterio(file, masked=True)
+da = rxr.open_rasterio(preflood_VV, masked=True)
 #da = da.rio.reproject("EPSG:4326")
-df = da[0].to_pandas()
-df['y'] = df.index
-df = pd.melt(df, id_vars='y')
+df_preflood_vv = da[0].to_pandas()
+df_preflood_vv['y'] = df_preflood_vv.index
+df_preflood_vv = pd.melt(df_preflood_vv, id_vars='y')
+df_preflood_vv=df_preflood_vv.rename(columns={'value':'preflood_vv'})
+
+da = rxr.open_rasterio(postflood_VV, masked=True)
+#da = da.rio.reproject("EPSG:4326")
+df_postflood_vv = da[0].to_pandas()
+df_postflood_vv['y'] = df_postflood_vv.index
+df_postflood_vv = pd.melt(df_postflood_vv, id_vars='y')
+df_postflood_vv=df_postflood_vv.rename(columns={'value':'postflood_vv'})
+
+da = rxr.open_rasterio(preflood_VH, masked=True)
+#da = da.rio.reproject("EPSG:4326")
+df_preflood_vh = da[0].to_pandas()
+df_preflood_vh['y'] = df_preflood_vh.index
+df_preflood_vh = pd.melt(df_preflood_vh, id_vars='y')
+df_preflood_vh=df_preflood_vh.rename(columns={'value':'preflood_vh'})
+
+da = rxr.open_rasterio(postflood_VV, masked=True)
+#da = da.rio.reproject("EPSG:4326")
+df_postflood_vh = da[0].to_pandas()
+df_postflood_vh['y'] = df_postflood_vh.index
+df_postflood_vh = pd.melt(df_postflood_vh, id_vars='y')
+df_postflood_vh=df_postflood_vh.rename(columns={'value':'postflood_vh'})
+
+""""
+merge data into one single pandas dataframe
+"""
+join1= pd.merge(df_postflood_vh, df_postflood_vv, on=['y', 'x'], how='inner')
+join2= pd.merge(df_preflood_vv, df_preflood_vh, on=['y', 'x'], how='inner')
+df = pd.merge(join1,join2, on=['y', 'x'], how='inner')
 df
 
-filtered_df = df[df['x'] >= -66.05833]
-filtered_df
+df['prefloodvv-postfloodvv'] = df['preflood_vv'] - df['postflood_vv']
+df
 
-da
+df['prefloodvh-postfloodvh'] = df['preflood_vh'] - df['postflood_vh']
+df
+""""
+not sure what to do with the data from here
+"""
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
-df2 = da[1].to_pandas()
-df2['y'] = df2.index
-df2 = pd.melt(df2, id_vars='y')
-df2
+# heatmap_data = df.pivot_table(index='y', columns='x', values='prefloodvh-postfloodvh')
 
-df3 = da[0].to_pandas()
-df3['y'] = df3.index
-df3 = pd.melt(df3, id_vars='y')
-df3
-
-
-
-da = rxr.open_rasterio(file2, masked=True)
-#da = da.rio.reproject("EPSG:4326")
-df22 = da[0].to_pandas()
-df22['y'] = df22.index
-df22 = pd.melt(df22, id_vars='y')
-df22
-
-df23 = da[1].to_pandas()
-df23['y'] = df23.index
-df23 = pd.melt(df23, id_vars='y')
-df23
-
-df24 = da[2].to_pandas()
-df24['y'] = df24.index
-df24 = pd.melt(df24, id_vars='y')
-df24
-
-# import rasterio
-# import pandas as pd
-# import numpy as np
-
-
-#     """
-#     Converts a specific band from a GeoTIFF to a Pandas DataFrame.
-
-#     Args:
-#         geotiff_path (str): Path to the GeoTIFF file.
-#         band_num (int): Band number to extract (1-based index).
-
-#     Returns:
-#         pandas.DataFrame: DataFrame with columns 'x', 'y', and 'value'.
-#                         Returns None if an error occurs.
-#     """
-
-
-# band_data = tiff.read(1)
-            
-#             # Create coordinate arrays
-# rows, cols = band_data.shape
-# x, y = np.meshgrid(np.arange(cols), np.arange(rows))
-            
-#             # Flatten arrays and create DataFrame
-# df_test = pd.DataFrame({'x': x.flatten(),'y': y.flatten(),'value': band_data.flatten()})
-# df_test
-
+# sns.heatmap(heatmap_data, annot=True, cmap='YlGnBu')
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.title('Heatmap of Values')
+# plt.show()
