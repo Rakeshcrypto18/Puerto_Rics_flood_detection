95c5e7a94be3e2583785e9282687253c8996e5cd
allison-forsyth <aforsyt4@gmu.edu>
2025-02-18 22:45:12 -0500

    adding some instructions to api call

--- process_api_connection.py
+++ process_api_connection.py
@@ -39,18 +39,6 @@ config.save("cdse")
 
 config = SHConfig("cdse")
 
-"""
-define bounding box coordinates of interest
-"""
-
-# coords_wgs84 = (-66.07507,18.41733,-66.058282,18.428126)
-
-
-resolution = 10
-# pr_bbox = BBox(bbox=coords_wgs84, crs=CRS.WGS84)
-# pr_size = bbox_to_dimensions(pr_bbox, resolution=resolution)
-
-print(f"Image shape at {resolution} m resolution: {pr_size} pixels")
 
 """
 create eval script to downolad and grab images of interest
@@ -74,7 +62,7 @@ function setup() {
 function evaluatePixel(samples) {
   const value = Math.max(0, Math.log(samples.VH) * 0.21714724095 + 1);
   return {
-    default: [value, value, samples.dataMask],
+    default: [value, value, value, samples.dataMask],
     eobrowserStats: [Math.max(-30, (10 * Math.log10(samples.VH)))],
     dataMask: [samples.dataMask],
   };
@@ -94,17 +82,30 @@ return [Math.max(0, Math.log(VH) * 0.21714724095 + 1)];
 """
 send request
 """
+# change coordinates by grid request
+# copy and pase from excel sheet be sure to include commas between values
+
+#this is changing depending on grid being pulled
 bbox = BBox(bbox=[-7377043.971, 2092185.762, -7372043.971, 2087185.762], crs=CRS.POP_WEB)
 
+#pre flood time: '2017-09-01', '2017-09-16'
+#post flood time: '2017-09-20', '2017-09-28'
+
+# namming convention
+# preflood_VV_gridnumber
+# postflood_VV_gridnumber
+# preflood_VH_gridnumber
+# postflood_VH_gridnumber
+
 request = SentinelHubRequest(
-    data_folder="preflood_VV_01",
+    data_folder="preflood_VV_01", #this is changing every pull
     evalscript=evalscript,
     input_data=[
         SentinelHubRequest.input_data(
             data_collection=DataCollection.SENTINEL1_IW.define_from(
                     "s1iw", service_url=config.sh_base_url
                 ),          
-            time_interval=('2017-09-01', '2017-09-16'),          
+            time_interval=('2017-09-01', '2017-09-16'), #this is chnaging depending on pre or post flood         
             other_args={"dataFilter": {"mosaickingOrder": "mostRecent"},"processing": {"orthorectify": True,"demInstance": "COPERNICUS","speckleFilter": {"type": "LEE","windowSizeX": 3,"windowSizeY": 3}}}
         ),
     ],
@@ -118,28 +119,6 @@ request = SentinelHubRequest(
 
 request.get_data()
 
-# request = SentinelHubRequest(
-#     data_folder="test",
-#     evalscript=evalscript,
-#     input_data=[
-#         SentinelHubRequest.input_data(
-#             data_collection=DataCollection.SENTINEL1_IW.define_from(
-#                     "s1iw", service_url=config.sh_base_url
-#                 ),          
-#             time_interval=('2017-09-01', '2017-09-16'),          
-#             other_args={"dataFilter": {"mosaickingOrder": "mostRecent","resolution": "HIGH"},"processing": {"orthorectify": True,"demInstance": "COPERNICUS","speckleFilter": {"type": "LEE","windowSizeX": 3,"windowSizeY": 3}}}
-#         ),
-#     ],
-#     responses=[
-#         SentinelHubRequest.output_response('default', MimeType.TIFF),
-#     ],
-#     bbox=pr_bbox,
-#     size=pr_size,
-#     config=config
-# )
-
-
-#response = request.get_data()
 """
 save geotiff to folder
 """
@@ -150,19 +129,7 @@ for folder, _, filenames in os.walk(request.data_folder):
     for filename in filenames:
         print(os.path.join(folder, filename))
 
-"""
-plot image
-"""
 
-imgs = request.get_data()
-
-image = imgs[0]
-print(f"Image type: {image.dtype}")
-
-# plot function
-# factor 1/255 to scale between 0-1
-# factor 3.5 to increase brightness
-#plot_image(image, factor=3.5 / 255, clip_range=(0, 1))
 
 """
 read geotiff as a dataframe
@@ -173,8 +140,19 @@ import rasterio.plot
 import matplotlib
 import rioxarray as rxr 
 
-file = 'test/85d04f52ed4133d55e72d84a476ef5a6/response.tiff'
+file = 'test/85d04f52ed4133d55e72d84a476ef5a6/response.tiff' #file path of the geotiff you saved
+#reccomend renaming response.tiff to the naming convention.tiff instead, we can then place all of our tiff images into a shared folder on git hub
 tiff = rasterio.open(file)
+tiff
+
+with rasterio.open(file) as src:
+    # Read a specific band (e.g., band 1)
+    band_number = 1
+    band_data = src.read(band_number)
+
+band_data
+
+#save it as a pandas data frame
 
 import pandas as pd
 da = rxr.open_rasterio(file, masked=True)
